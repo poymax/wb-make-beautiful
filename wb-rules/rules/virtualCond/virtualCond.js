b105eff51a863e82a2d3ce2a9ccbd636cb08da13
dfedotov <dfedotov@lanta-net.ru>
2022-10-20 20:04:17 +0300

    Corrected virtualCond rule

--- wb-rules/rules/virtualCond/virtualCond.js
+++ wb-rules/rules/virtualCond/virtualCond.js
@@ -1,6 +1,8 @@
 // Виртуальный кондиционер
 
-var config = readConfig('/etc/wb-rules/virtualCond/virtualCondLinks.conf')
+var PATH_TO_CONFIG = '/etc/wb-rules/virtualCond/virtualCondLinks.conf'
+
+var config = readConfig(PATH_TO_CONFIG)
 
 defineVirtualDevice('virtualCond', {
     title: 'Virtual Conditioner',
@@ -16,7 +18,13 @@ defineVirtualDevice('virtualCond', {
 
 var conditioner = defineRule({
     whenChanged: 'virtualCond/mode',
-    then: function(newValue) {
-        dev[config["commands"][newValue]] = 1
+    then: function(command) {
+        commands = config['commands']
+
+        if (command in commands) {
+            dev[commands[command]] = true
+        } else {
+            log.warning('Command "{}" not found in {}', command, PATH_TO_CONFIG)
+        }
     }
 })
