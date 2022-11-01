9ca16bc5586994f5da19fa33454b247696d6b266
dfedotov <dfedotov@lanta-net.ru>
2022-11-01 18:05:00 +0300

    Working on virtual AC detector

---
+++ wb-rules/rules/virtualACdetector/virtualACdetector.js
@@ -0,0 +1,50 @@
+// Virtual AC detector
+
+var PATH_TO_CONFIG = '/etc/wb-rules/virtualACdetector/config.conf'
+
+var config = readConfig(PATH_TO_CONFIG)
+
+defineVirtualDevice('virtualACdetector', {
+    title: 'Virtual AC detector',
+    cells: {
+        Alarm: {
+            type: 'alarm',
+            value: false,
+            readonly: false,
+            order: 1,
+        },
+    }
+})
+
+function setACalarm(status) {
+    dev['virtualACdetector/Alarm'] = status
+}
+
+defineRule('virtualACalarm', {
+    whenChanged: [
+        config['topic'] + '#error',
+        config['topic']
+    ],
+    then: function(newValue) {
+        switch(config['type']) {
+            case 'value':
+                // TODO
+                break
+            case 'boolean':
+                // TODO
+                break
+            case 'error':
+                // TODO
+                log(newValue)
+                if (newValue !== '') {
+                    setACalarm(true)
+                } else {
+                    setACalarm(false)
+                }
+                break
+            default:
+                log.error('Wrong type "{}" in {}. Type must be "value", "boolean" or "error"', config['type'], PATH_TO_CONFIG);
+                break
+        }
+    }
+})
