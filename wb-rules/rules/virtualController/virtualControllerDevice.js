8fa80842b68ac07e9a1055362add2187fb3792b7
dfedotov <dfedotov@lanta-net.ru>
2022-11-21 17:38:52 +0300

    Added critical overheat setpoints

--- wb-rules/rules/virtualController/virtualControllerDevice.js
+++ wb-rules/rules/virtualController/virtualControllerDevice.js
@@ -99,65 +99,79 @@ defineVirtualDevice('virtualController', {
             order: 14,
         },
 
+      	tIN_overheat_critical: {
+            type: 'temperature',
+            value: 35,
+            readonly: false,
+            order: 15,
+        },
+
+      	tIN_overheat_critical_delta: {
+            type: 'temperature',
+            value: 3,
+            readonly: false,
+            order: 16,
+        },
+        
         tIN_heating: {
             type: 'temperature',
             value: 13,
             readonly: false,
-            order: 15,
+            order: 17,
         },
 
         tIN_heating_delta: {
             type: 'temperature',
             value: 3,
             readonly: false,
-            order: 16,
+            order: 18,
         },
 
         tOUT_winKit: {
             type: 'temperature',
             value: 2,
             readonly: false,
-            order: 17,
+            order: 19,
         },
 
         tOUT_winKit_delta: {
             type: 'temperature',
             value: 2,
             readonly: false,
-            order: 18,
+            order: 20,
         },
 
         tOUT_switching: {
             type: 'temperature',
             value: 20,
             readonly: false,
-            order: 19,
+            order: 21,
         },
 
         tOUT_switching_delta: {
             type: 'temperature',
             value: 2,
             readonly: false,
-            order: 20,
+            order: 22,
         },
 
         Humidity_threshold: {
             type: 'rel_humidity',
             value: 80,
             readonly: false,
-            order: 21,
+            order: 23,
         },
 
         Humidity_threshold_delta: {
             type: 'rel_humidity',
             value: 20,
             readonly: false,
-            order: 22,
+            order: 24,
         },
 
         Restart_rules: {
             type: 'pushbutton',
-            order: 23
+            order: 25
         },
     },
 })
