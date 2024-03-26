803418b8ac33a4650b470f5b48c92b4e81578dc8
dfedotov <dfedotov@lanta-net.ru>
2024-03-26 13:57:36 +0300

    Add Out_temp_calc_Device

---
+++ wb-rules/Out_temp_calc_Device.js
@@ -0,0 +1,22 @@
+defineVirtualDevice('Out_temp_calc', {
+    title: 'Out_temp_calc',
+    cells: {
+        Fix_power_fan: {
+            type: 'switch',
+            value: false,
+            readonly: false,
+            order: 1,
+        },
+        V_fan: {
+            type: 'value',
+            readonly: false,
+            value: 0,
+            order: 3,
+        },
+        t_out: {
+            type: 'value',
+            value: 0,
+            order: 4,
+        },
+    },
+})
---
+++ wb-rules/Out_temp_calc_rules.js
@@ -0,0 +1,49 @@
+var fullPowerTopic = 'wb-map6s_24/P 1'
+var cool1PowerTopic = 'wb-map6s_24/P 3'
+var cool2PowerTopic = 'wb-map6s_24/P 4'
+var fanCurrentTopic = 'wb-map6s_24/Irms 2'
+
+var fixPowerFanEnableTopic = 'Out_temp_calc/Fix_power_fan'
+var fanPowerValueTopic = 'Out_temp_calc/Fan_power'
+var vFanValueTopic = 'Out_temp_calc/V_fan'
+var outTempTopic = 'Out_temp_calc/t_out'
+
+var tInTopic = 'virtualController/tINTopic_cooling'
+
+function fixPowerFan(enabled) {
+    var device = getDevice('Out_temp_calc')
+
+    if (enabled) {
+        device.addControl('Fan_power', {
+            type: 'value',
+            value: '50',
+            readonly: false,
+            order: 2,
+        })
+    } else {
+        device.removeControl('Fan_power')
+    }
+
+    setOutTemp()
+}
+
+function setOutTemp() {
+    fanCurrentFormula = dev[fixPowerFanEnableTopic] ? dev[fanPowerValueTopic] : dev[fanCurrentTopic] * 220
+    dev[outTempTopic] = Math.floor(dev[tInTopic] - ((dev[fullPowerTopic] - dev[cool1PowerTopic] - dev[cool2PowerTopic] - fanCurrentFormula) / 1000 / (dev[vFanValueTopic] / 1.1 / 3600) * 1.2 * 1.02))
+}
+
+defineRule('recalcOutTemp', {
+    whenChanged: [
+        fullPowerTopic,
+        fanPowerValueTopic,
+        vFanValueTopic,
+    ],
+    then: setOutTemp,
+})
+
+defineRule('fixPowerFan', {
+    whenChanged: fixPowerFanEnableTopic,
+    then: fixPowerFan,
+})
+
+fixPowerFan(dev[fixPowerFanEnableTopic])
