6f87d9f8c7d34c77e0acd58bd2d6eeb5bb5fb33c
poymax <poymax@yandex.ru>
2024-05-14 16:17:18 +0300

    add low temp to alarm bot

--- wb-rules/bot/bot.js
+++ wb-rules/bot/bot.js
@@ -7,13 +7,17 @@ var chatId = config['chatId'];
 var messageThreadId = config['messageThreadId'];
 var fireAlarmTopic = 'virtualFireDetector/fire';
 var doorAlarmTopic = 'virtualDoor/Door';
-var acAlarmTopic = 'virtualACdetector/AC';
-var tBoxTopic = 'alarm/high_temp';
+var acAlarmTopic = 'virtualACdetector/AC_alarm';
+var city = config['city'];
+var generator = config['generator'];
+var tBoxLowTopic = 'alarm/low_temp';
+var tBoxHighTopic = 'alarm/high_temp';
 
 function checkAlarm() {
     var command = 'curl -s -X POST https://api.telegram.org/bot{}/sendMessage -d chat_id={} -d message_thread_id={} -d text={}'.format(token, chatId, messageThreadId, message);
 runShellCommand(command);
 }
+
 if (doorAlarmTopic) {
   var doorAlarmRule = defineRule("door", {
    whenChanged: doorAlarmTopic,
@@ -26,7 +30,7 @@ if (doorAlarmTopic) {
 }
 if (city) {
   var acAlarmRule = defineRule("AC", {
-   whenChanged: city,
+   whenChanged: acAlarmTopic,
     then: function () {
       if (dev[city] == true & dev[acAlarmTopic] == true) {
         message = "Отклонение\\ напряжения\\ городской\\ сети";
@@ -48,13 +52,23 @@ if (fireAlarmTopic) {
   });
   runRule(fireAlarmRule)
 }
-if (tBoxTopic) {
-  var tBoxRule = defineRule("temp", {
-    whenChanged: tBoxTopic,
+if (tBoxLowTopic) {
+  var tBoxLowRule = defineRule("lowTemp", {
+    whenChanged: tBoxLowTopic,
     then: function () {
-        message = dev[tBoxTopic] ? "Жарко!!!" : 'Температура\\ в\\ норме!';
+        message = dev[tBoxLowTopic] ? "Холодно!!!" : 'Температура\\ в\\ норме!';
       checkAlarm()
       }
   });
-  runRule(tBoxRule)
+  runRule(tBoxLowRule)
+}
+if (tBoxHighTopic) {
+  var tBoxHighRule = defineRule("highTemp", {
+    whenChanged: tBoxHighTopic,
+    then: function () {
+        message = dev[tBoxHighTopic] ? "Жарко!!!" : 'Температура\\ в\\ норме!';
+      checkAlarm()
+      }
+  });
+  runRule(tBoxHighRule)
 }
